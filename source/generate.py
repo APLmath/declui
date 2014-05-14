5d9c92e878f4253ca101ebcc926e46c36475dacf
Andrew Lee <alee@eecs.berkeley.edu>
2014-05-13 22:52:25 -0700

    Meh, text_element kind of working....

--- source/generate.py
+++ source/generate.py
@@ -8,17 +8,20 @@ WS = ~"[ \\t\\r\\n]+"
 """
 
 DATA_GRAMMAR = """
-class_decl_list = WS? class_decl (WS class_decl)* WS?
-class_decl      = "class" WS class_name WS "{" field_decl_list? "}"
+class_decl_list = WS? (class_decl WS?)*
+class_decl      = "class " class_name " {" field_decl_list? "}"
 field_decl_list = WS? field_decl (WS field_decl)* WS?
 field_decl      = field_type WS field_name ";"
 field_type      = "bool" / "int" / "string" / class_name
 """ + COMMON_GRAMMAR
 
 TEMPLATE_GRAMMAR = """
-template_decl_list = WS? template_decl (WS template_decl) * WS?
-template_decl      = "{template " class_name "." template_name "}" WS? "{/template}"
-template_name      = ~"[a-z]+([A-Z][a-z]*)*" 
+template_decl_list = WS? (template_decl WS?)*
+template_decl        = "{template " class_name "." template_name "}" element_list "{/template}"
+template_name        = ~"[a-z]+([A-Z][a-z]*)*"
+element_list         = (div_element / text_element / WS)*
+div_element          = "<div>" element_list "</div>"
+text_element         = ~"[A-Z0-9][A-Z 0-9]*[A-Z0-9]|[A-Z0-9]"i
 """ + COMMON_GRAMMAR
 
 SAMPLE_DATA = """
@@ -38,6 +41,11 @@ class Employee {
 
 SAMPLE_TEMPLATE = """
 {template Employee.businessCard}
+<div>
+  <div>
+    Hello there
+  </div>
+</div>
 {/template}
 """
 

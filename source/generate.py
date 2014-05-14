6a34e7ff618274f7d8f62d0c10df37cf7bb0ca35
Andrew Lee <alee@eecs.berkeley.edu>
2014-05-14 00:13:46 -0700

    expr grammar kind of working...

--- source/generate.py
+++ source/generate.py
@@ -1,29 +1,42 @@
 import parsimonious
 
-COMMON_GRAMMAR = """
+COMMON_GRAMMAR_TEXT = """
 class_name      = ~"[A-Z][a-z]*"
 field_name      = ~"[a-z]+(_[a-z]+)*"
+
+int_literal     = ~"-?\\d+"
+atom            = "this" / int_literal / ("(" expr ")")
+expr            = or_test
+or_test         = and_test / (or_test "||" and_test)
+and_test        = not_test / (and_test "&&" not_test)
+not_test        = comparison / ("!" not_test)
+comparison      = a_expr / (a_expr ("<" / "<=" / ">" / ">=" / "==" / "!=") a_expr)
+a_expr          = m_expr / (a_expr ("+" / "-") m_expr)
+m_expr          = primary / (m_expr "*" primary)
+primary         = atom / attribute_ref
+attribute_ref   = primary "." field_name
 
 WS = ~"[ \\t\\r\\n]+"
 """
 
-DATA_GRAMMAR = """
+DATA_GRAMMAR_TEXT = """
 class_decl_list = WS? (class_decl WS?)*
 class_decl      = "class " class_name " {" field_decl_list? "}"
 field_decl_list = WS? field_decl (WS field_decl)* WS?
 field_decl      = field_type WS field_name ";"
 field_type      = "bool" / "int" / "string" / class_name
-""" + COMMON_GRAMMAR
+""" + COMMON_GRAMMAR_TEXT
 
-TEMPLATE_GRAMMAR = """
+TEMPLATE_GRAMMAR_TEXT = """
 template_decl_list = WS? (template_decl WS?)*
 template_decl      = "{template " class_name "." template_name "}" element_list "{/template}"
 template_name      = ~"[a-z]+([A-Z][a-z]*)*"
-element_list       = (div_element / if_element / text_element / WS)*
+element_list       = (div_element / if_element / val_element / text_element / WS)*
 div_element        = "<div>" element_list "</div>"
 if_element         = "{if}" element_list ("{else}" element_list)? "{/if}"
-text_element       = ~"[A-Z0-9][A-Z 0-9]*[A-Z0-9]|[A-Z0-9]"i
-""" + COMMON_GRAMMAR
+val_element        = "{{" expr "}}"
+text_element       = ~"[A-Z 0-9]+"i
+""" + COMMON_GRAMMAR_TEXT
 
 SAMPLE_DATA = """
 class Address {
@@ -44,14 +57,14 @@ SAMPLE_TEMPLATE = """
 {template Employee.businessCard}
 <div>
   <div>
-    Hello there
+    {{this.name}}
     {if}
-      Blah
+      Blah   
     {/if}
   </div>
 </div>
 {/template}
 """
 
-DATA_GRAMMAR = parsimonious.grammar.Grammar(DATA_GRAMMAR)
-TEMPLATE_GRAMMAR = parsimonious.grammar.Grammar(TEMPLATE_GRAMMAR)
+DATA_GRAMMAR = parsimonious.grammar.Grammar(DATA_GRAMMAR_TEXT)
+TEMPLATE_GRAMMAR = parsimonious.grammar.Grammar(TEMPLATE_GRAMMAR_TEXT)
